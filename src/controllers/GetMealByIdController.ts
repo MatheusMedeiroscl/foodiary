3db7336944fa5f6948c6341b0f6472921828383d
MatheusMedeiroscl <medeiros.matheuscl@gmail.com>
2025-07-25 09:30:25 -0300

    buscar meal por ID

---
+++ src/controllers/GetMealByIdController.ts
@@ -0,0 +1,42 @@
+import { date, z } from 'zod';
+
+import { db } from '../db';
+import { mealsTable } from '../db/schema';
+import { HttpResponse, ProtectedHttpRequest } from '../types/Http';
+import { badRequest, ok } from '../utils/http';
+import { eq, and, gte, lte } from 'drizzle-orm';
+
+
+const schema = z.object({
+    mealId: z.uuid(),
+})
+
+
+export class GetMealByIdController {
+    static async handle({userId, params}: ProtectedHttpRequest): Promise<HttpResponse>{
+        const {success, error, data} = schema.safeParse(params)
+        
+        if(!success){
+            return badRequest({errors: error.issues})
+        }
+
+
+        const meal = await db.query.mealsTable.findFirst({
+            columns: {
+                id: true,
+                foods: true,
+                createdAt: true,
+                icon: true,
+                name: true,
+                status: true
+            },
+             where: and (
+                    eq(mealsTable.userId, userId),
+                    eq(mealsTable.id, data.mealId),
+                    
+                )
+        });
+
+        return ok({meal})
+    }
+}
